26382c130a83405c2c3eaa9c2a0b12af8f19b2e1
LunarX <gibran.chevalley@etu.unige.ch>
2020-03-03 16:24:35 +0100

    Added yanagiba swift-ast depedency to the package.swift and copied yanagiba's code in main

--- Package.swift
+++ Package.swift
@@ -8,13 +8,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/yanagiba/swift-ast.git", from: "0.19.9")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "pattern-extraction-swift",
-            dependencies: []),
+            dependencies: ["SwiftAST+Tooling"]),
         .testTarget(
             name: "pattern-extraction-swiftTests",
             dependencies: ["pattern-extraction-swift"]),
--- Sources/pattern-extraction-swift/main.swift
+++ Sources/pattern-extraction-swift/main.swift
@@ -1,1 +1,18 @@
-print("Hello, world!")
+import AST
+import Parser
+import Source
+
+print("main has been run")
+
+do {
+  let sourceFile = try SourceReader.read(at: "../../swift_files/Exercice1.swift")
+  let parser = Parser(source: sourceFile)
+  let topLevelDecl = try parser.parse()
+
+  for stmt in topLevelDecl.statements {
+    // consume statement
+    print(stmt)
+  }
+} catch {
+  // handle errors
+}
