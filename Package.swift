d05a999845a161fcca9c46084d0ad0ca2d6ba473
LunarX <gibran.chevalley@etu.unige.ch>
2020-03-03 16:12:38 +0100

    Redid the package init

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "pattern-extraction-swift",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "pattern-extraction-swift",
-            targets: ["pattern-extraction-swift"]),
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
--- Sources/pattern-extraction-swift/pattern_extraction_swift.swift
+++
@@ -1,3 +0,0 @@
-struct pattern_extraction_swift {
-    var text = "Hello, World!"
-}
