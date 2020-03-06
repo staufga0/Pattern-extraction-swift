2eec3945de202b838f8d51b99557c25d777e348a
LunarX <gibran.chevalley@etu.unige.ch>
2020-03-06 11:37:20 +0100

    tried some stmt visit. added type_inferance.swift file to work on

--- Sources/pattern-extraction-swift/main.swift
+++ Sources/pattern-extraction-swift/main.swift
@@ -4,27 +4,50 @@ import Source
 
 print("main has been run")
 
-let sourceFile = try SourceReader.read(at: "swift_files/Exercice1.swift")
-let parser = Parser(source: sourceFile)
-let topLevelDecl = try parser.parse()
+var nb = 0
+var nb2 = 0
 
-for stmt in topLevelDecl.statements {
-  // consume statement
-  print(stmt)
+do {
+  let sourceFile = try SourceReader.read(at: "swift_files/type_inferance.swift")
+  let parser = Parser(source: sourceFile)
+  // let topLevelDecl = try parser.parse()
+
+
+  class MyVisitor : ASTVisitor {
+    func visit(_ stmt: ConstantDeclaration) throws -> Bool {
+      // visit this if statement
+      print("Found const")
+
+      nb += 1
+      return true
+    }
+
+    func visit(_ stmt: VariableDeclaration) throws -> Bool {
+      print("Found var")
+
+      switch stmt.Body {
+      case .initializerList(let a):
+        print(stmt.textDescription)
+      default:
+        print("Got default case")
+      }
+
+      nb2 += 1
+      return true
+    }
+  }
+  let myVisitor = MyVisitor()
+  let topLevelDecl = try parser.parse()
+  try myVisitor.traverse(topLevelDecl)
+
+  print(nb)
+
+  // for stmt in topLevelDecl.statements {
+  //   // consume statement
+  //   print("=============\n", stmt, "\n=============\n")
+  //   print(type(of: stmt))
+  // }
+} catch {
+  // handle errors
+  print("error: \(error).")
 }
-
-// do {
-//   let sourceFile = try SourceReader.read(at: "../../swift_files/Exercice1.swift")
-//   let parser = Parser(source: sourceFile)
-//   let topLevelDecl = try parser.parse()
-//
-//   print("topLevelDecl")
-//   print(topLevelDecl)
-//
-//   for stmt in topLevelDecl.statements {
-//     // consume statement
-//     print(stmt)
-//   }
-// } catch {
-//   // handle errors
-// }
---
+++ swift_files/type_inferance.swift
@@ -0,0 +1,5 @@
+var a = 0
+let b = 2
+let c :Int
+var d :Float
+var e = 3.1416
