2f9c41210455a1266e6b4bb1378ceed9e394e8a2
LunarX <gibran.chevalley@etu.unige.ch>
2020-05-16 21:29:54 +0200

    Computed results for 75 first repositories

--- Sources/pattern-extraction-swift/main.swift
+++ Sources/pattern-extraction-swift/main.swift
@@ -1,9 +1,13 @@
 let pe = PatternExtractor()
 
-pe.applyToOne("swift_files/optional.swift", debug: true)
-pe.applyToAll("swift_files/files/", "./out_data.json", "./all_keys.txt")
+// pe.applyToOne("swift_files/optional.swift", debug: true)
+// pe.applyToAll("swift_files/files/", "./out_data.json", "./all_keys.txt")
 
 
 // pe.applyToAll("/media/gibran/Extreme%20SSD/Pattern_files/files/", "./out_data.json", "./all_keys.txt")
 // pe.applyToAll("/media/gibran/Extreme%20SSD/Pattern_files/files20_1/", "./out_data_1.json", "./all_keys_1.txt")
-// pe.applyToAll("/media/gibran/Extreme%20SSD/Pattern_files/files20_5/", "./out_data_5.json", "./all_keys_5.txt")
+// pe.applyToAll("/media/gibran/Extreme%20SSD/Pattern_files/files20_2/", "./out_data_2.json", "./all_keys_2.txt")
+// pe.applyToAll("/media/gibran/Extreme%20SSD/Pattern_files/files20_3/", "./out_data_3.json", "./all_keys_3.txt")
+pe.applyToAll("/media/gibran/Extreme%20SSD/Pattern_files/files20_4/", "./out_data_4.json", "./all_keys_4.txt")
+pe.applyToAll("/media/gibran/Extreme%20SSD/Pattern_files/files20_5/", "./out_data_5.json", "./all_keys_5.txt")
+pe.applyToAll("/media/gibran/Extreme%20SSD/Pattern_files/files20_6/", "./out_data_6.json", "./all_keys_6.txt")
