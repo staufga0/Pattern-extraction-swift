3555254fc1efe1921e5a96e04ceb6f66215746d7
staufga0 <Guy-Raphael.Stauffer@etu.unige.ch>
2020-05-18 14:06:18 +0200

    add computation of mean and std

--- data_reader/data_reader.py
+++ data_reader/data_reader.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import json
 import os
+import numpy as np
 
 data = {}
 
@@ -12,31 +13,49 @@ try:
 except IOError as e:
           print('Operation failed: %s' % e.strerror)
 
-
-
+nbProjet = 0
+stats = {}
 for name, rep in data.items():
     print('')
     print('')
     print(name)
     for repo, com in rep.items():
-        c = 0
-
-        vd_time = []
-        vd_nb = []
-
-        for commit, count in com.items():
-            vd_time.append(int(commit))
-            if "class declaration" in count :
-                vd_nb.append(count["class declaration"])
+        nbProjet += 1
+        commit, count = sorted(com.items(), reverse=True)[0]
+        for key in count:
+            if key in stats:
+                stats[key].append(count[key])
             else :
-                vd_nb.append(0)
-            if c == 0 :
-                for pattern, number in count.items():
-                    print(pattern)
-                c = 1
-        plt.figure()
-        plt.plot(vd_time, vd_nb,'o')
-        plt.title(repo)
-        plt.ylabel("number of class declaration")
-        plt.show()
-        # print(repo)
+                stats[key] = [count[key]]
+
+for key in stats :
+    print(key, ' mean: ', np.mean(stats[key]))
+    print(key, ' std: ', np.std(stats[key]))
+
+print("nb projet: ", nbProjet)
+# for name, rep in data.items():
+#     print('')
+#     print('')
+#     print(name)
+#     for repo, com in rep.items():
+#         c = 0
+#
+#         vd_time = []
+#         vd_nb = []
+#
+#         for commit, count in com.items():
+#             vd_time.append(int(commit))
+#             if "class declaration" in count :
+#                 vd_nb.append(count["class declaration"])
+#             else :
+#                 vd_nb.append(0)
+#             if c == 0 :
+#                 for pattern, number in count.items():
+#                     print(pattern)
+#                 c = 1
+#         plt.figure()
+#         plt.plot(vd_time, vd_nb,'o')
+#         plt.title(repo)
+#         plt.ylabel("number of class declaration")
+#         plt.show()
+#         # print(repo)
